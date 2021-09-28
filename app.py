8f789babb75741454428c66c5beeee2e114227bc
Zoe Corbier <zoe.corbier@outlook.fr>
2021-09-28 14:34:55 +0200

    score correction

--- app.py
+++ app.py
@@ -1,54 +1,71 @@
 import json
-
 from spellchecker import SpellChecker
 
-mailText = '{"objet" : "Support NETFLIX", "text": "Bonjour , Nous n\'avons pas pu autoriser votre paiement pour le prochain cycle de facturation de votre abonnement. Nous serions bien évidemment très heureux de vous compter à nouveau parmi nous. cliquez simplement sur, réactivez simplement votre abonnement pour profiter des meilleurs films et séries TV sans interruption. RÉACTIVER L\'ABONNEMENT. Nous sommes là pour vous aider. Pour plus d\'informations, consultez le Centre d\'aide ou contactez-nous. L\'équipe Netflix"}'
+our_mail = '{"objet" : "Support NETFLIX", "text": "Bonjour , Nous n\'avons pas pu autoriser votre paiement pour le prochain cycle de facturation de votre abonnement. Nous serions bien évidemment très heureux de vous compter à nouveau parmi nous. cliquez simplement sur, réactivez simplement votre abonnement pour profiter des meilleurs films et séries TV sans interruption. RÉACTIVER L\'ABONNEMENT. Nous sommes là pour vous aider. Pour plus d\'informations, consultez le Centre d\'aide ou contactez-nous. L\'équipe Netflix"}'
 
 
-# parse json:
-text = json.loads(mailText)
+def Mail_ML (mail_recup):
+
+    '''
+    La fonction prends en paramètre un Json avec ce que l'on récupère de l'utilisateur 
+
+    Elle rends un json avec les scores calculés pendant le machine learning sur la probabilité que le mail soit un faux 
+
+    On prends en compte :
+
+        - Les fautes d'orthographes présentent dans le mail 
+    '''
+    # parse json:
+    text = json.loads(mail_recup)
 
 
-#Objet a corriger
-#print(text["text"])
-mail_text=text["text"]
+    #Objet a corriger
+    corps_mail=text["text"]
 
-
-
-new_text=""
-foundForbiddenChar = False
-for i in range(0, len(mail_text), 1):
-    # end of text mail
-    if (i+1 == len(mail_text)):
-        new_text = new_text + mail_text[i]
-        break
-    
-    if (mail_text[i+1]!="'" and mail_text[i+1]!=","):
-        if not foundForbiddenChar :
-            new_text += mail_text[i]
-            foundForbiddenChar = False
+    new_str=""
+    foundForbiddenChar = False
+    for i in range(0, len(corps_mail)-1, 1):
+        # end of corps_mail
+        if (i+1 == len(corps_mail)):
+            new_str = new_str + corps_mail[i]
+           
+        if (corps_mail[i+1]!="'" and corps_mail[i+1]!=","):
+            if not foundForbiddenChar :
+                new_str += corps_mail[i]
+                foundForbiddenChar = False
+            else :
+                foundForbiddenChar = False
         else :
-            foundForbiddenChar = False
-    else :
-        foundForbiddenChar = True
+            foundForbiddenChar = True
 
 
-print(new_text)
+    # print(new_str)
 
         
 
-spell = SpellChecker(language='fr')
-# print(spell.correction("bojour"))
+    spell = SpellChecker(language='fr')
+    # print(spell.correction("bojour"))
 
-l=mail_text.split()
+    all_words=new_str.split()
 
-new_l=[]
-for i in range (0,len(l),1):
+    corrected_all_words=[]
+    for i in range (0,len(all_words),1):
 
-    #print(spell.correction(l[i]))
-    new_l.append(spell.correction(l[i]))
+        #print(spell.correction(l[i]))
+        corrected_all_words.append(spell.correction(all_words[i]))
 
 
-print (new_l)
+    # print (corrected_all_words)
 
-print('{"score1": "15%", "scores2": "80%"}')
+    count_errors=0
+    for i in range (0,len(corrected_all_words),1):
+        if (corrected_all_words[i]!=all_words[i]):
+            count_errors+=1
+
+    print ("Il y a " + str(count_errors) + " erreurs")
+
+    print('{"score1": "15%", "scores2": "80%"}')
+
+
+
+Mail_ML(our_mail)
