cc632b0093e0f8d1afeba008cdd188075efca0bf
Zoe Corbier <zoe.corbier@outlook.fr>
2021-09-28 10:38:25 +0200

    Correction d'une liste

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
 import json
 
+from spellchecker import SpellChecker
 mailText = '{"objet" : "Support NETFLIX", "text": "Bonjour , Nous n\'avons pas pu autoriser votre paiement pour le prochain cycle de facturation de votre abonnement. Nous serions bien évidemment très heureux de vous compter à nouveau parmi nous. cliquez simplement sur, réactivez simplement votre abonnement pour profiter des meilleurs films et séries TV sans interruption. RÉACTIVER L\'ABONNEMENT. Nous sommes là pour vous aider. Pour plus d\'informations, consultez le Centre d\'aide ou contactez-nous. L\'équipe Netflix"}'
 
 
 # parse json:
 text = json.loads(mailText)
 
-# the result is a Python dictionary:
-#print(text["text"]) 
+
+#Objet a corriger
+#print(text["text"])
+mail_text=text["text"]
+
+
+spell = SpellChecker(language='fr')
+# print(spell.correction("bojour"))
+
+l=mail_text.split()
+# print(l)
+
+for i in range (0,len(l),1):
+    print(spell.correction(l[i]))
+
 
 print('{"score1": "15%", "scores2": "80%"}')
