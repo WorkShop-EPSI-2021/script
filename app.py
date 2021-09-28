a412c39c0e1f42144b7cd4d4135f04cbde11ea48
Zoe Corbier <zoe.corbier@outlook.fr>
2021-09-28 12:08:12 +0200

    Correction p2

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import json
 
 from spellchecker import SpellChecker
+
 mailText = '{"objet" : "Support NETFLIX", "text": "Bonjour , Nous n\'avons pas pu autoriser votre paiement pour le prochain cycle de facturation de votre abonnement. Nous serions bien évidemment très heureux de vous compter à nouveau parmi nous. cliquez simplement sur, réactivez simplement votre abonnement pour profiter des meilleurs films et séries TV sans interruption. RÉACTIVER L\'ABONNEMENT. Nous sommes là pour vous aider. Pour plus d\'informations, consultez le Centre d\'aide ou contactez-nous. L\'équipe Netflix"}'
 
 
@@ -13,14 +14,30 @@ text = json.loads(mailText)
 mail_text=text["text"]
 
 
+
+new_text=str()
+for i in range(0,len(mail_text),1):
+    if (mail_text[i+1]!="'"):
+        print(mail_text[i])
+        # new_text+=mail_text[i]
+print(new_text)
+
+        
+
+
 spell = SpellChecker(language='fr')
 # print(spell.correction("bojour"))
 
 l=mail_text.split()
-# print(l)
+print(l)
 
+new_l=[]
 for i in range (0,len(l),1):
-    print(spell.correction(l[i]))
 
+    #print(spell.correction(l[i]))
+    new_l.append(spell.correction(l[i]))
+
+
+print (new_l)
 
 print('{"score1": "15%", "scores2": "80%"}')
