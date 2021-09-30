0fd03f39c2e9be84469763e654a9a81ddef94b55
Alexandrp2 <undercoverword@gmail.com>
2021-09-30 10:30:35 +0200

    Upadate feeling anlyzer

--- feelingAnalyzer.py
+++ feelingAnalyzer.py
@@ -111,7 +111,7 @@ class FeelingAnalyzer:
         X_train, X_test, y_train, y_test = train_test_split(
             x_features_mailText, 
             y_labels_feelings, 
-            test_size=0.2, 
+            test_size=0.1, 
             random_state=0
         )
 
@@ -169,7 +169,7 @@ class FeelingAnalyzer:
         X_train, X_test, y_train, y_test = train_test_split(
             x_features_mailText, 
             y_labels_feelings, 
-            test_size=0.2, 
+            test_size=0.1, 
             random_state=0
         )
 
@@ -228,7 +228,7 @@ class FeelingAnalyzer:
         X_train, X_test, y_train, y_test = train_test_split(
             x_features_mailText, 
             y_labels_feelings, 
-            test_size=0.2, 
+            test_size=0.1, 
             random_state=0
         )
 
